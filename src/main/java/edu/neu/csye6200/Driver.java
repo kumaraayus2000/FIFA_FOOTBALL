6c1b13464f724651dd51c49b184ae517a10c7046
aayushkumar <kumar.aayus@northeastern.edu>
2024-11-20 21:23:34 -0500

    Spring set up

--- src/main/java/edu/neu/csye6200/Driver.java
+++ src/main/java/edu/neu/csye6200/Driver.java
@@ -1,17 +1,21 @@
 package edu.neu.csye6200;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 
  * @author Gokul Jayavel
  * 
  */
 
+@SpringBootApplication
 public class Driver {
 	public static void main(String[] args) {
 		System.out.println("============Main Execution Start===================\n\n");
 
          //Add your code in between these two print statements
-
+		SpringApplication.run(Driver.class, args);
 
 
 		System.out.println("\n\n============Main Execution End===================");
