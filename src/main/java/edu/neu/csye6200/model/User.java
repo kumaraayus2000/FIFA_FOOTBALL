50a1a17ebf4b10d927d0dce7e29a6f61c05cf121
aayushkumar <kumar.aayus@northeastern.edu>
2024-12-09 08:39:58 -0500

    Making changes in user model.

--- src/main/java/edu/neu/csye6200/model/User.java
+++ src/main/java/edu/neu/csye6200/model/User.java
@@ -9,59 +9,55 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "user", schema = "public")
 public class User {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-
 	private String name;
-
 	private int age;
-
 	private String nationality;
-
 	private String position;
-
+//	public User(int id, String name, int age, String nationality, String position) {
+//		this.id = id;
+//		this.name = name;
+//		this.age = age;
+//		this.nationality = nationality;
+//		this.position = position;
+//	}
+//	public User(int id, int age, String nationality, String position) {
+//		this.id = id;
+//		this.age = age;
+//		this.nationality = nationality;
+//		this.position = position;
+//	}
 	// Getters and Setters
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public String getNationality() {
 		return nationality;
 	}
-
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
 	}
-
 	public String getPosition() {
 		return position;
 	}
-
 	public void setPosition(String position) {
 		this.position = position;
 	}
-
 }
